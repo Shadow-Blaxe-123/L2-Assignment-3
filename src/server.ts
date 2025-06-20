2df503707da8c7546c1ee50c556026ea32fb78cd
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-06-21 00:02:46 +0600

    Connected to Atlas from mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,20 @@
-import app from "./app";
+import server from "./app";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+dotenv.config();
+
+const { MONGODB_URL, PORT } = process.env;
+
+server.listen(PORT, () => {
+  console.log("Server is running on http://localhost:" + PORT);
 });
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(MONGODB_URL as string);
+  console.log("Database connected");
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
