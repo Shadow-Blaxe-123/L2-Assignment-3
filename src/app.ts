7c4e963e378752a8ba52bcdeb849156af193db3f
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-06-20 22:47:27 +0600

    basic borrow routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import GenericError from "./customError";
 import bookRouter from "./app/routes/bookRoute";
+import borrowRouter from "./app/routes/borrowRouter";
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/books", bookRouter);
+app.use("/api/borrow", borrowRouter);
 
 // 404 Error handler.
 app.use((req: Request, res: Response, next: NextFunction) => {
--- src/app/routes/borrowRouter.ts
+++ src/app/routes/borrowRouter.ts
@@ -1,5 +1,13 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const borrowRouter = express.Router();
+
+borrowRouter.post("/", (req: Request, res: Response) => {
+  res.status(200).send("Borrow Route post method");
+});
+
+borrowRouter.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Borrow Route get method");
+});
 
 export default borrowRouter;
