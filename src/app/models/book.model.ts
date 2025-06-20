241d5ba3525d09d2dbe799af1db99fe5eeda21cb
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-06-21 00:19:46 +0600

    Added Book Model

---
+++ src/app/models/book.model.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import { Book } from "./interfaces";
+
+const bookSchema = new mongoose.Schema<Book>({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+    enum: [
+      "FICTION",
+      "NON_FICTION",
+      "SCIENCE",
+      "HISTORY",
+      "BIOGRAPHY",
+      "FANTASY",
+    ],
+  },
+  isbn: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: String,
+  copies: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const BookModel = mongoose.model<Book>("Book", bookSchema);
+export default BookModel;
